6c226cba4832475bf3fac1edfbad140daeee334c
Andy Xu <andyxukq@gmail.com>
2024-10-12 22:13:14 +0800

    update

--- js/animations.js
+++ js/animations.js
@@ -1,76 +1,95 @@
 /* Dynamic background from Vanta */
 window.addEventListener("load", () => {
     /* Partical Cloud */
-    const canvas = document.getElementById('particleCanvas');
-    const ctx = canvas.getContext('2d');
+    const canvasEl = document.getElementById('particleCanvas');
+    const canvas =
+      'OffscreenCanvas' in window
+        ? canvasEl.transferControlToOffscreen()
+        : canvasEl;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    let particles = [];
-    let particleCount = calculateParticleCount();
+    const worker_js = `
+    onmessage = (e)=> {
+        const canvas = e.data.canvas;
+        const ctx = canvas.getContext('2d');
+        
 
-    class Particle {
-        constructor() {
-            this.reset();
-            this.y = Math.random() * canvas.height;
-            this.fadeDelay = Math.random() * 600 + 100;
-            this.fadeStart = Date.now() + this.fadeDelay;
-            this.fadingOut = false;
-        }
+        let particles = [];
+        let particleCount = calculateParticleCount();
 
-        reset() {
-            this.x = Math.random() * canvas.width;
-            this.y = Math.random() * canvas.height;
-            this.speed = Math.random() / 5 + 0.1;
-            this.opacity = 1;
-            this.fadeDelay = Math.random() * 600 + 100;
-            this.fadeStart = Date.now() + this.fadeDelay;
-            this.fadingOut = false;
-        }
-
-        update() {
-            this.y -= this.speed;
-            if (this.y < 0) {
+        class Particle {
+            constructor() {
                 this.reset();
+                this.y = Math.random() * canvas.height;
+                this.fadeDelay = Math.random() * 600 + 100;
+                this.fadeStart = Date.now() + this.fadeDelay;
+                this.fadingOut = false;
             }
 
-            if (!this.fadingOut && Date.now() > this.fadeStart) {
-                this.fadingOut = true;
+            reset() {
+                this.x = Math.random() * canvas.width;
+                this.y = Math.random() * canvas.height;
+                this.speed = Math.random() / 5 + 0.1;
+                this.opacity = 1;
+                this.fadeDelay = Math.random() * 600 + 100;
+                this.fadeStart = Date.now() + this.fadeDelay;
+                this.fadingOut = false;
             }
-            
-            if (this.fadingOut) {
-                this.opacity -= 0.008;
-                if (this.opacity <= 0) {
+
+            update() {
+                this.y -= this.speed;
+                if (this.y < 0) {
                     this.reset();
                 }
+
+                if (!this.fadingOut && Date.now() > this.fadeStart) {
+                    this.fadingOut = true;
+                }
+                
+                if (this.fadingOut) {
+                    this.opacity -= 0.008;
+                    if (this.opacity <= 0) {
+                        this.reset();
+                    }
+                }
+            }
+
+            draw() {
+                ctx.fillStyle = 'rgba(' + (255 - (Math.random() * 255 / 2)) + ', 255, 255, ' + this.opacity + ')';
+                ctx.fillRect(this.x, this.y, 0.6, Math.random() * 2 + 1);
             }
         }
 
-        draw() {
-            ctx.fillStyle = `rgba(${255 - (Math.random() * 255/2)}, 255, 255, ${this.opacity})`;
-            ctx.fillRect(this.x, this.y, 0.6, Math.random() * 2 + 1);
+        function initParticles() {
+            particles = [];
+            for (let i = 0; i < particleCount; i++) {
+                particles.push(new Particle());
+            }
         }
-    }
 
-    function initParticles() {
-        particles = [];
-        for (let i = 0; i < particleCount; i++) {
-            particles.push(new Particle());
+        function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            particles.forEach(particle => {
+                particle.update();
+                particle.draw();
+            });
+            requestAnimationFrame(animate);
         }
-    }
 
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        particles.forEach(particle => {
-            particle.update();
-            particle.draw();
-        });
-        requestAnimationFrame(animate);
-    }
+        function calculateParticleCount() {
+            return Math.floor((canvas.width * canvas.height) / 6000);
+        }   
 
-    function calculateParticleCount() {
-        return Math.floor((canvas.width * canvas.height) / 6000);
-    }
+        initParticles();
+        animate();
+    }`;
+
+    const blob = new Blob([worker_js], { type: 'text/javascript' });
+    const url = URL.createObjectURL(blob);
+    const worker = new Worker(url);
+    worker.postMessage({ canvas: canvas }, [canvas]);
+    URL.revokeObjectURL(url);
 
     function onResize() {
         canvas.width = window.innerWidth;
@@ -79,11 +98,9 @@ window.addEventListener("load", () => {
         initParticles();
     }
 
-    window.addEventListener('resize', onResize);
+    //window.addEventListener('resize', onResize);
 
-    initParticles();
-    animate();
-
+    
     /* Dot Lottie */
     jQuery.getScript("assets/dotlottie-web.js", ()=>{
         new DotLottie({
